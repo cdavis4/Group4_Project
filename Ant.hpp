6f6497e1aa2ab7b79c43fb78a36e2b10a2ce2e5a
Carrie Davis <carrie_davis@esri.com>
2018-07-25 13:55:31 -0700

    Added Ant Class
    I created a basic Ant class to start on. Using enum TYPE right now to identify the critters. This is what I did with my Project 2.

---
+++ Ant.hpp
@@ -0,0 +1,31 @@
+//
+//  Ant.hpp
+//  
+//
+//  Created by Carrie Davis on 7/25/18.
+//
+
+#ifndef Ant_hpp
+#define Ant_hpp
+
+#include "Critter.hpp"
+class Ant: public Critter
+{
+protected:
+ 
+public:
+    
+    Ant(int inRow, int inColumn):Critter(row, column)
+    {
+        
+    }
+    Ant();
+    void move();
+    Ant* breed(int, int);
+    TYPE getType();
+    
+    
+    
+};
+
+#endif /* Ant_hpp */
