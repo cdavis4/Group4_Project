aa82db722ffbc75008744647cf93a3a6b21544ad
patrickbyrne1 <byrnepat@oregonstate.edu>
2018-07-26 17:35:50 -0500

    changed DoodleBug to inherit from Critter, not Ant

--- DoodleBug.cpp
+++ DoodleBug.cpp
@@ -6,7 +6,12 @@
 //
 
 #include "DoodleBug.hpp"
-#include "Ant.hpp"
+
+
+DoodleBug::DoodleBug()
+{}
+
+
 /*************************************************
  * DoodleBug::getType()
  * gets the type of critter
@@ -15,6 +20,9 @@ TYPE DoodleBug::getType()
 {
     return DOODLE;
 }
+
+
+
 /*************************************************
  * Ant::eat()
  * gets the type of critter
@@ -25,15 +33,15 @@ void DoodleBug::eat(Ant* inAntLocation)
     
 }
 /*********************************************************
- * Critter::move
+ * Critter::move -> this should be DoodleBug, not Critter right?
  * Moves critter one step at its current position
  * changing its position
  * place the Doodle at its new position.
  ****************************************************/
-void Critter::move(int inRow, int inColumn)
+void DoodleBug::move(int &inRow, int &inColumn)
 {
     //could randomize here
-    getPostion(inRow,inColumn);  //not sure if this is just an unnecessary step
+    //getPostion(inRow,inColumn);  //not sure if this is just an unnecessary step
     //do checks for a doodle bug
     setPosition(inRow,inColumn);
   //maybe call eat? change to take in row, column? or 2D array?
--- DoodleBug.hpp
+++ DoodleBug.hpp
@@ -9,18 +9,20 @@
 #define DoodleBug_hpp
 
 #include "Critter.hpp"
-class DoodleBug:: public Ant
+#include "Ant.hpp"
+// doodlebug needs to inherit from Critter, not Ant
+class DoodleBug: public Critter //(ME: fixed extra colon -> it was Doodlebug:: not Doodlebug:
 {
 protected:
 
 public:
-    DoodleBug(int inRow, int inColumn):Ant(row, column)
+    DoodleBug(int inRow, int inColumn):Critter(row, column)
     {
         
     }
     DoodleBug();
     void move(int&, int&); //Same as ant except no survive
-    void eat(int, int);  //now this should just get Ant* and delete.
+    void eat(Ant*);  //now this should just get Ant* and delete.
     TYPE getType();
     
 };
