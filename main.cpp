24995278041aacb0a2f267ee171fe5aa4ab207cf
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-25 21:46:11 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 #include "Menu.hpp"
-
+#include "Board.hpp"
 using namespace std;
 
 // For Initial Testing Purposes
 
 int main()
-{
+{	
 	Menu menu;
-	
+	int ants = menu.getAnts();
+	int doodlebugs = menu.getDoodlebugs();
+	int steps = menu.getSteps();
+	int row = menu.getRow();
+	int col = menu.getCol();
+	Board board1(row, col);
 }
