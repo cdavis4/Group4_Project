7b874cf3dbb52ecd0e8392b860e63343237b3737
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-25 21:43:45 -0500

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,9 +1,13 @@
 #include "Board.hpp"
 Board::Board(int row, int col)
 {
+	this -> row = row;
+	this -> col = col;
+	
 	boardArray = new Critter **[row];
 	for(int i = 0; i < col; i++)
 	{
 		boardArray[i] = new Critter*;
 	}
+	
 }
