6f1f70b57fbdce73fb5d653c816f9efa5da9e487
patrickbyrne1 <41702877+patrickbyrne1@users.noreply.github.com>
2018-07-27 14:43:25 -0500

    Update Critter.hpp

--- Critter.hpp
+++ Critter.hpp
@@ -16,13 +16,13 @@ protected:
   //use with friends and derived
     int row;
     int column;
-	int survive;
-	int moveCount; //Added to prevent a critter being moved more than once during move loop.
+    int survive;
+    int moveCount; //Added to prevent a critter being moved more than once during move loop.
 			
 
-	virtual void move();
+    virtual void move();
     virtual void move(int, int); //virutal function
-    
+    virtual void move(int inRow, int inColumn, Critter*** cBoard, int row, int col);
     //updated these
     void getPosition(int&,int&);
     void setPosition(int, int);
@@ -30,7 +30,7 @@ public:
     virtual TYPE getType() {return NONE;}
     Critter();
     Critter(int inRow, int inColumn);
-	void setSurvive(int);
-	void resetMoveCount();
+    void setSurvive(int);
+    void resetMoveCount();
 };
 #endif /* Critter_hpp */
