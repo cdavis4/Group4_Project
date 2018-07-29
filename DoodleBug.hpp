62192f454d6e03676c9e45d40caed1f57dd7c082
patrickbyrne1 <41702877+patrickbyrne1@users.noreply.github.com>
2018-07-29 14:13:38 -0500

    Update DoodleBug.hpp

--- DoodleBug.hpp
+++ DoodleBug.hpp
@@ -14,13 +14,15 @@
 class DoodleBug: public Critter 
 {
 protected:
-
+   int survive;
 public:
    
     DoodleBug();
     void move(int&, int&); //Same as ant except no survive
     void eat(Ant*);  //now this should just get Ant* and delete.
     TYPE getType();
+    void move(int inRow, int inColumn, Critter*** cBoard, int row, int col);
+    void breed(int inRow, int inColumn, Critter*** cBoard, int row, int col);
     
 };
 #endif /* DoodleBug_hpp */
