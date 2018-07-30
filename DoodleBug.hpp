1d8f689e29803979ff45431aa851bd87e77b49a1
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-29 22:18:29 -0500

    Update DoodleBug.hpp

--- DoodleBug.hpp
+++ DoodleBug.hpp
@@ -1,10 +1,10 @@
-//
-//  DoodleBug.hpp
-//  
-//
-//  Created by Carrie Davis on 7/25/18.
-//
-
+/*******************************************************
+Doodlebug is a derived class of Critter. Its move method
+looks for an Ant to eat in an adjacent cell, if not,
+it moves randomly. If the Doodlebug does not eat an
+Ant in three steps, then the Doodlebug starves and
+dies.
+********************************************************/
 #ifndef DoodleBug_hpp
 #define DoodleBug_hpp
 
@@ -13,13 +13,11 @@
 // doodlebug needs to inherit from Critter, not Ant
 class DoodleBug: public Critter 
 {
-protected:
-   int survive;
-public:
-   
+ protected:
+    int survive;
+
+ public:
     DoodleBug();
-    void move(int&, int&); //Same as ant except no survive
-    void eat(Ant*);  //now this should just get Ant* and delete.
     TYPE getType();
     void move(int inRow, int inColumn, Critter*** cBoard, int row, int col);
     void breed(int inRow, int inColumn, Critter*** cBoard, int row, int col);
