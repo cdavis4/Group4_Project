474f2771a453ff5d8c414c72d50829ed1d3e7c80
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-29 22:42:49 -0500

    Update Critter.cpp

--- Critter.cpp
+++ Critter.cpp
@@ -25,22 +25,6 @@ Critter::Critter()
 	starveCount = 0;
 }
 
-/*********************************************************
- * Critter::move
- * Moves critter one step FROM its current position
- * changing its position
- * place the CRITTER at its new position.
- ****************************************************/
-
-void Critter::move(int inRow, int inColumn, Critter*** cBoard, int row, int col)
-{
-	
-}
-
-/*************************************************
- * Ant::getSurvive()
- * gets survival days of Ant
- **************************************************/
 void Critter::setSurvive(int inSurvive)
 {
     survive = inSurvive;
@@ -56,10 +40,6 @@ int Critter::getStarveCount()
 }
 
 
-void Critter::breed(int inRow, int inColumn, Critter*** cBoard, int row, int col)
-{
-	
-}
 /*********************************************************
 starve checks if a doodlebug has eaten an ant in the past
 three steps, if not, it deletes the doodlebug.
