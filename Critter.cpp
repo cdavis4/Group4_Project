46ae1c3dc69c834338c2dc36ad42ca2a9ef5b163
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-29 22:15:39 -0500

    Update Critter.cpp

--- Critter.cpp
+++ Critter.cpp
@@ -1,8 +1,18 @@
-/*******************************************************
- ** Author: carrie davis
- ** Date:July 25, 2018
- ** Description: Group 4 Project; Critter
- ******************************************************/
+/***********************************************************************
+  Critter is the parent class of the derived classes Ant and Doodlebug. 
+ It has the following protected data members: survive which counts 
+ the number of steps the Critter object has lived through and is used
+ for determining if the Critter should breed and for the Doodlebug’s
+ starve method, and moveCount that increments when a critter has moved. 
+ Critter has the following public methods: a virtual method called move 
+ that moves the placement of a Critter object depending on the type and 
+ arrangement of the board when called, a virtual method called breed 
+ that creates a new derived Critter object if the selected Critter is 
+ old enough, a virtual method called starve that checks if a Doodlebug 
+ has eaten an ant in the last three steps and deletes the Doodlebug if it 
+ has not, and a virtual function called type that returns the type of the
+ Critter as assigned by an enum in the Critter class.
+*************************************************************************/
 #include <iostream>
 using namespace std;
 #include "Critter.hpp"
