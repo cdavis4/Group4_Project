45f8823d52b540c5681c355c8130fe7071a6810c
roemenm <41705715+roemenm@users.noreply.github.com>
2018-07-29 22:15:06 -0500

    Update Critter.hpp

--- Critter.hpp
+++ Critter.hpp
@@ -1,8 +1,18 @@
-//
-//  Critter.hpp
-//  Created by Carrie Davis on 7/25/18.
-//
-
+/***********************************************************************
+  Critter is the parent class of the derived classes Ant and Doodlebug. 
+ It has the following protected data members: survive which counts 
+ the number of steps the Critter object has lived through and is used
+ for determining if the Critter should breed and for the Doodlebug’s
+ starve method, and moveCount that increments when a critter has moved. 
+ Critter has the following public methods: a virtual method called move 
+ that moves the placement of a Critter object depending on the type and 
+ arrangement of the board when called, a virtual method called breed 
+ that creates a new derived Critter object if the selected Critter is 
+ old enough, a virtual method called starve that checks if a Doodlebug 
+ has eaten an ant in the last three steps and deletes the Doodlebug if it 
+ has not, and a virtual function called type that returns the type of the
+ Critter as assigned by an enum in the Critter class.
+*************************************************************************/
 #ifndef Critter_hpp
 #define Critter_hpp
 
@@ -12,28 +22,20 @@ enum TYPE{DOODLE,ANT,NONE}; // type of animals that can be added
 
 class Critter
 {
-protected:
-  //use with friends and derived
-    int row;
-    int column;
+ protected:
     int survive;
-    int moveCount; //Added to prevent a critter being moved more than once during move loop.
-   int starveCount;
-
-
-    //updated these
-   // void getPosition(int&,int&);
-   // void setPosition(int, int);
-public:
+    int moveCount; 
+    int starveCount;
+	
+ public:
     virtual void move(int inRow, int inColumn, Critter*** cBoard, int row, int col);
     virtual TYPE getType() {return NONE;}
     Critter();
     virtual void starve(int inRow, int inColumn, Critter*** cBoard, int row, int col);
-    Critter(int , int ); // represents survive and moveCount
     void setSurvive(int);
     void resetMoveCount();
     void resetStarve();
-	int getStarveCount();
+    int getStarveCount();
     virtual void breed(int inRow, int inColumn, Critter*** cBoard, int row, int col);
 	
 };
